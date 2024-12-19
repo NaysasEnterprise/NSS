6fe532b6523acf1ac25f3cf3ac787d1053b4d39d
Artem <blinowarteom@icloud.com>
2024-12-19 14:43:07 +0300

    task 7.1

--- ProgramFiles/NSS/server/SQL_header.h
+++ ProgramFiles/NSS/server/SQL_header.h
@@ -5,33 +5,39 @@
 
 namespace SQL_Database {
 
-    // Добавление нового пользователя в базу данных
+    //для пользователей
     void addUser(sqlite3* db, const std::string& name, const std::string& password);
-
-    // Вход пользователя с проверкой имени и пароля
     bool loginUser(sqlite3* db, const std::string& name, const std::string& password);
-
-    // Удаление пользователя по имени
     void deleteUser(sqlite3* db, const std::string& name);
-
-    // Отображение всех пользователей
     void displayUsers(sqlite3* db);
 
-    // Выполнение SQL скрипта
-    void executeSQLScript(sqlite3* db, const std::string& scriptPath);
-
-    // Обработка HTTP запроса (для различных операций)
-    void handleHttpRequest(sqlite3* db, int clientSocket, const std::string& httpRequest);
-
-    // Вспомогательные функции для обработки пользователей
+    //вспомогательные функции для обработки пользователей
     void handleAddUser(sqlite3* db, int clientSocket, const std::string& body);
     void handleLoginUser(sqlite3* db, int clientSocket, const std::string& body);
     void handleDeleteUser(sqlite3* db, int clientSocket, const std::string& body);
     void handleDisplayUsers(sqlite3* db, int clientSocket);
 
+    // Выполнение SQL скрипта
+    void executeSQLScript(sqlite3* db, const std::string& scriptPath);
+    // Обработка HTTP запроса (для различных операций)
+    void handleHttpRequest(sqlite3* db, int clientSocket, const std::string& httpRequest);
     // Вспомогательная функция для отправки HTTP ответа
     void sendHttpResponse(int clientSocket, const std::string& status, const std::string& body);
-
     // Вспомогательная функция для извлечения данных из тела запроса
     std::string extractData(const std::string& body, const std::string& key);
+    bool isNumeric(const std::string& str);
+
+
+    //для пунктов выдачи
+    void addPickupPoint(sqlite3* db, const std::string& name, const std::string& address, double coordX, double coordY);
+    void editPickupPoint(sqlite3* db, int pickupPointID, const std::string& name, const std::string& address, double coordX, double coordY);
+    void deletePickupPoint(sqlite3* db, int pickupPointID);
+    void displayPickupPoints(sqlite3* db);
+
+    //вспомогательные для пунктов выдачи
+    void handleAddPickupPoint(sqlite3* db, int clientSocket, const std::string& body);
+    void handleEditPickupPoint(sqlite3* db, int clientSocket, const std::string& body);
+    void handleDeletePickupPoint(sqlite3* db, int clientSocket, const std::string& body);
+    void handleDisplayPickupPoints(sqlite3* db, int clientSocket);
+
 }
